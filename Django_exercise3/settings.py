cded09c25ead1b5a2508a8e9bc9e7932081a77aa
wszeborj <wszeborj@o2.pl>
2023-06-18 00:04:11 +0200

    created first simple template for dashboard

--- Django_exercise3/settings.py
+++ Django_exercise3/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users',
+    'dashboard.apps.DashboardConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
